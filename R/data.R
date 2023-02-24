d6f1d504bd230bdc53c08ea3c4896b303390dc0b
Andrew Siefert <andrewsiefert@gmail.com>
2023-02-24 12:07:34 -0500

    Add Greece forests dataset

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Greece forests composition data
+#'
+#' Community composition data from the "Greece forests" dataset
+#'
+#' @docType data
+#' @name greece
+#' @usage data(greece)
+#' @format A data frame with 385 rows and 3 columns:
+#' \itemize{
+#'   \item PlotObservationID
+#'   \item Species
+#'   \item Abundance
+#' }
+NULL
